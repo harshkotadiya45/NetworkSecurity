20b8a9e7e03b6dbdf4a084c21ebc09ba2ffd4f74
harshkotadiya45 <23dcs051@charusat.edu.in>
2025-11-05 18:35:31 +0530

    Exception.py

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -1,8 +1,8 @@
 import logging
 import os
-import datetime
+from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime("%m_%d_%Y_%H_%m_%S")}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
